0b3c675d56c5c36a154623cf3ab45fb52f07165b
Aleksander Pentchev <alek.pentchev@gmail.com>
2022-07-02 22:45:30 +0200

    exclude

--- 43-exclude.ts
+++ 43-exclude.ts
@@ -1,3 +1,6 @@
 // Implement the built-in Exclude<T, U>
 // Exclude from T those types that are assignable to U
 
+type MyExclude<T, U> = T extends U ? never : T
+
+type Bar = MyExclude<'a' | 'b' | 'c', 'a' | 'c'>
