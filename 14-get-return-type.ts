a2bf383b79b02b1be2ca752525ec4ba67a5939a8
Aleksander Pentchev <alek.pentchev@gmail.com>
2022-07-03 12:46:33 +0200

    implemented ReturnType

---
+++ 14-get-return-type.ts
@@ -0,0 +1,12 @@
+// Implement the built-in ReturnType<T> generic without using it.
+
+const fn = (v: boolean) => {
+    if (v)
+        return 1
+    else
+        return 2
+}
+
+type MyReturnType<T> = T extends (...args: any[]) => infer R ? R : never
+
+type a = MyReturnType<typeof fn> // should be "1 | 2"
